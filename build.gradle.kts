04e7412ae6d9d11c2fb10d1503f862351f01e951
Philip Graf <git@acanda.ch>
2023-08-19 11:06:32 +0200

    Set up Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
 plugins {
     kotlin("jvm")
     `java-gradle-plugin`
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
 }
 
 group = "ch.acanda.gradle"
@@ -25,10 +29,16 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+detekt {
+    buildUponDefaultConfig = true // preconfigure defaults
+    config.setFrom("$projectDir/config/detekt.yaml")
 }
 
 testing {
@@ -49,5 +59,19 @@ testing {
 tasks {
     wrapper {
         gradleVersion = "8.2.1"
+    }
+
+    withType<Detekt>().configureEach {
+        val compilation =
+            project.extensions.getByType<KotlinJvmProjectExtension>().target.compilations.getByName("test")
+        classpath.setFrom(compilation.output.classesDirs, compilation.compileDependencyFiles)
+
+        reports {
+            xml.required.set(false)
+            html.required.set(false)
+            txt.required.set(false)
+            sarif.required.set(false)
+            md.required.set(true)
+        }
     }
 }
