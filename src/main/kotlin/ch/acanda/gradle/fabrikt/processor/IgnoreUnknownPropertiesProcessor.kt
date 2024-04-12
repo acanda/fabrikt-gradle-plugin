02ad7f69f95777252eeea6e77ec015fc13441bcc
Philip Graf <git@acanda.ch>
2024-04-12 16:45:36 +0200

    Lock file when adding JsonIgnoreProperties

--- src/main/kotlin/ch/acanda/gradle/fabrikt/processor/IgnoreUnknownPropertiesProcessor.kt
+++ src/main/kotlin/ch/acanda/gradle/fabrikt/processor/IgnoreUnknownPropertiesProcessor.kt
@@ -1,13 +1,14 @@
 package ch.acanda.gradle.fabrikt.processor
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.extension
 import kotlin.io.path.isRegularFile
-import kotlin.io.path.readText
 import kotlin.io.path.walk
-import kotlin.io.path.writeText
 
 @OptIn(ExperimentalPathApi::class)
 fun addIgnoreUnknownPropertiesAnnotation(modelsDirectory: Path) {
@@ -17,11 +18,30 @@ fun addIgnoreUnknownPropertiesAnnotation(modelsDirectory: Path) {
         .forEach { addAnnotation(it) }
 }
 
-private fun addAnnotation(file: Path) {
+private fun addAnnotation(path: Path) {
+    // Exclusively lock the file to prevent concurrent writes.
+    FileChannel.open(path, StandardOpenOption.READ, StandardOpenOption.WRITE).use { channel ->
+        channel.lock().use {
+            addAnnotation(channel)
+        }
+    }
+}
+
+private fun addAnnotation(channel: FileChannel) {
     val import = "import ${JsonIgnoreProperties::class.qualifiedName}".replace("annotation", "`annotation`")
-    val annotation = "@${JsonIgnoreProperties::class.simpleName}(${JsonIgnoreProperties::ignoreUnknown.name} = true)"
-    val annotatedClass = file.readText()
+    val annotation =
+        "@${JsonIgnoreProperties::class.simpleName}(${JsonIgnoreProperties::ignoreUnknown.name} = true)"
+    val dataClass = "public data class"
+    val buffer = ByteBuffer.allocate(channel.size().toInt())
+    channel.read(buffer)
+    val content = String(buffer.array(), Charsets.UTF_8)
+    if (content.contains(annotation) || !content.contains(dataClass)) {
+        return
+    }
+    val annotatedClass = content
         .replaceFirst("import", "$import\nimport")
-        .replace("public data class", "$annotation\npublic data class")
-    file.writeText(annotatedClass)
+        .replace(dataClass, "$annotation\npublic data class")
+    channel.position(0)
+    channel.write(ByteBuffer.wrap(annotatedClass.toByteArray()))
+    channel.force(true)
 }
