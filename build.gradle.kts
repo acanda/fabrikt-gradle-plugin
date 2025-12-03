4e5ff5d905df97f40730ca5158222b0c0dd87ad0
Philip Graf <git@acanda.ch>
2025-12-03 20:54:42 +0100

    Update to next snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "ch.acanda.gradle"
-version = "1.25.0"
+version = "1.26.0-SNAPSHOT"
 
 val generatedSources: Provider<Directory> = project.layout.buildDirectory.dir("generated/src/main/kotlin")
 
