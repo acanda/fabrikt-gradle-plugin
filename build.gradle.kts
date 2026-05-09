1ecbb3d1d880db2c766fda30f57abbc3d9e42a21
Philip Graf <git@acanda.ch>
2026-05-09 13:25:51 +0200

    Release 1.34.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "ch.acanda.gradle"
-version = "1.35.0-SNAPSHOT"
+version = "1.34.1"
 
 val generatedSources: Provider<Directory> = project.layout.buildDirectory.dir("generated/src/main/kotlin")
 
