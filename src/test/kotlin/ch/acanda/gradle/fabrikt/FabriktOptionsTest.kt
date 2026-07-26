1e861de0a4058915efa0863314fb6b82af1a56fd
Philip Graf <git@acanda.ch>
2026-07-26 14:23:01 +0200

    Add tests for missing InstantLibrary and TypeOverrides options

--- src/test/kotlin/ch/acanda/gradle/fabrikt/FabriktOptionsTest.kt
+++ src/test/kotlin/ch/acanda/gradle/fabrikt/FabriktOptionsTest.kt
@@ -1,14 +1,23 @@
 package ch.acanda.gradle.fabrikt
 
 import com.cjbooms.fabrikt.cli.ClientCodeGenTargetType
+import com.cjbooms.fabrikt.cli.CodeGenTypeOverride
 import com.cjbooms.fabrikt.cli.ControllerCodeGenTargetType
 import com.cjbooms.fabrikt.cli.ExternalReferencesResolutionMode
+import com.cjbooms.fabrikt.cli.InstantLibrary
 import com.cjbooms.fabrikt.cli.JacksonNullabilityMode
 import com.cjbooms.fabrikt.cli.SerializationLibrary
 import com.cjbooms.fabrikt.cli.ValidationLibrary
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.JsonNodeType
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
+import java.io.File
 import kotlin.reflect.KClass
 
 class FabriktOptionsTest : StringSpec({
@@ -35,6 +44,29 @@ class FabriktOptionsTest : StringSpec({
 
     "JacksonNullabilityMode" {
         JacksonNullabilityModeOption::class shouldMatch JacksonNullabilityMode::class
+    }
+
+    "InstantLibrary" {
+        InstantLibraryOption::class shouldMatch InstantLibrary::class
+    }
+
+    "TypeOverrides" {
+        val schema: ObjectNode =
+            ObjectMapper(YAMLFactory()).registerKotlinModule()
+                .readTree(File("src/main/schema/configuration.yaml")) as ObjectNode
+
+        val options = schema.withObject("options")
+            .filter { it.get("source").textValue() == "com.cjbooms.fabrikt.cli.CodeGenTypeOverride" }
+            .flatMap { option ->
+                option.withObject("mapping")
+                    .valueStream()
+                    .filter { it.nodeType != JsonNodeType.NULL }
+                    .map { it.textValue() }
+                    .toList()
+            }
+            .toSet()
+
+        options shouldContainExactlyInAnyOrder CodeGenTypeOverride.entries.map { it.name }.toSet()
     }
 
 }) {
